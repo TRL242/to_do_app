b182142f71d70bb888b9e68aae6344612b897e21
trl242 <royaurallockhart@gmail.com>
2021-04-02 12:45:21 -0700

    updating todo page

--- djangotodo/todo/views.py
+++ djangotodo/todo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import TodoForm
+from .models import Todo
+
+def todos(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+
+            return redirect('todos')
+    else:
+        form = TodoForm()
+    
+    todos = Todo.objects.all()
+    
+    return render(request, 'todos.html', {'todos': todos, 'form': form})
